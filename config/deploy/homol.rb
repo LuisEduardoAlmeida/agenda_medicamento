c117f5d3c6032f4133d6fa1783102656ce492db4
LuisEduardoAlmeida <leduardoalmeida@hotmail.com>
2017-12-01 13:49:20 -0300

    Iniciando projeto

---
+++ config/deploy/homol.rb
@@ -0,0 +1,8 @@
+set :domain       , '178.62.63.159'
+set :user         , 'eduardo'
+
+set :deploy_to    , '/home/eduardo/agenda_medicamento'
+set :repository   , 'git@github.com:LuisEduardoAlmeida/agenda_medicamento.git'
+set :branch       , 'feature-clonar_ppa'
+
+set :rails_env    , 'homologacao'
---
+++ config/deploy/production.rb
@@ -0,0 +1,6 @@
+set :domain       , '178.62.63.159'
+set :user         , 'eduardo'
+
+set :deploy_to    , '/home/eduardo/agenda_medicamento'
+set :repository   , 'git@github.com:LuisEduardoAlmeida/agenda_medicamento.git'
+set :branch       , 'production'
