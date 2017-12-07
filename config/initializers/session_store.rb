7a7c918130ccacfd5c528efb62164ee987532509
LuisEduardoAlmeida <leduardoalmeida@hotmail.com>
2017-12-07 17:04:40 -0300

    Configurando devise para autenticar

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_agenda_medicamento_session'
+if Rails.env.production?
+  AgendaMedicamento::Application.config.session_store :cookie_store, key: '_agenda_medicamento_session', domain: '178.62.63.159/'
+elsif Rails.env.homologacao?
+ AgendaMedicamento::Application.config.session_store :cookie_store, key: '_agenda_medicamento_session_dev', domain: '178.62.63.159/'
+elsif Rails.env.development?
+  AgendaMedicamento::Application.config.session_store :cookie_store, key: '_agenda_medicamento_session_dev', domain: :all
+end
